a350078d50d4adf3a6419a787117fb07493b2eda
Adam Martin <adam.martin@homeinsteadinc.com>
2016-01-08 10:45:13 -0600

    Can successfully set up a SQLServer using powershell ... I hate you powershell

--- create_environment.ps1
+++ create_environment.ps1
@@ -1,17 +1,35 @@
-Function GetOrCreate-ResourceGroup([string]$name = "", [string]$location = "")
-{
+Function AuthenticateToSubscription([string]$username = "", [string]$password = "", [string]$subscriptionName = "") {
+     $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+     $cred = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+     Login-AzureRmAccount -Credential $cred -SubscriptionName $subscriptionName
+     $sub = Get-AzureRmSubscription -subscriptionName $subscriptionName
+     Select-AzureRmSubscription -SubscriptionId $sub.SubscriptionId
+}
+
+Function GetOrCreate-ResourceGroup([string]$name = "", [string]$location = "") {
    If($name -eq "" -Or $location -eq "") {
-      Write-Host "USAGE:\n\tPS > . [script_location]\create_environment.ps1; GetOrCreateResoureGroup -name [NameOfResourceGroup] -location [LocationToInstall e.g. "Central US"]"
+      Write-Host "USAGE:\n\tPS > . [script_location]\create_environment.ps1; GetOrCreate-ResoureGroup -name [NameOfResourceGroup] -location [LocationToInstall e.g. 'Central US']"
       return
    }
    
    try {
-      return Get-AzureRmResourceGroup -name $name
+      $result = Get-AzureRmResourceGroup -name $name
+      return $result.ResourceGroupName
    }
    catch {
-      Write-Host "I WOULD DO: New-AzureRmResourceGroup -Name $name -Location $location"
-      return $result
+      $result = New-AzureRmResourceGroup -Name $name -Location $location
+      return $result.ResourceGroupName
    }
+}
+
+Function Create-SqlServer([string]$group = "", [string]$serverName = "", [string]$databaseName = "", [string]$administratorLogin = "", [string]$administratorLoginPassword = "", [string]$serverVersion="12.0") {
+   If($group -eq "" -Or $serverName -eq "" -Or $databaseName -eq "" -Or $administratorLogin -eq "" -Or $administratorLoginPassword -eq "") {
+      Write-Host "USAGE:\n\tPS > . [script_location]\create_environment.ps1; Create-SqlServer -group [group from GetOrCreate-ResourceGroup] -serverName [NameOfSQLServer] -databaseName [NameOfDatabase] -administratorLogin [AdminLogin] -administratorLoginPassword [AdminPassword]"
+      return
+   }
+
+   $secpasswd = ConvertTo-SecureString $administratorLoginPassword -AsPlainText -Force
+   New-AzureRmResourceGroupDeployment -ResourceGroupName $group -TemplateFile .\sqlServerDeploy.json -serverName $serverName -serverVersion $serverVersion -databaseName $databaseName -administratorLogin $administratorLogin -administratorLoginPassword $secpasswd
 }
 
 
